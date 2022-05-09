e08ec606625974a5deba304643d4d7ae3acfbcd6
Nguyen Tuan Linh <tuanlinh.nguyen@dsquare.com.vn>
2022-05-09 11:22:31 +0700

    upgrade

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -11,6 +11,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
