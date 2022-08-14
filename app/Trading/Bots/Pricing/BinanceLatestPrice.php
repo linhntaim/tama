6bb40d2bedda67eeff50887e0a271f82a4266a67
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2022-08-15 01:15:27 +0700

    fix

--- app/Trading/Bots/Pricing/BinanceLatestPrice.php
+++ app/Trading/Bots/Pricing/BinanceLatestPrice.php
@@ -6,9 +6,9 @@ use App\Trading\Bots\Exchanges\Binance;
 
 class BinanceLatestPrice extends LatestPrice
 {
-    public function __construct(string $ticker, Interval|string $interval, array $actionPrice = [])
+    public function __construct(string $ticker, Interval|string $interval, array $price = [])
     {
-        parent::__construct(Binance::NAME, $ticker, $interval, $actionPrice);
+        parent::__construct(Binance::NAME, $ticker, $interval, $price);
     }
 
     public function getTime(): int
